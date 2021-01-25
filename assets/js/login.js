4a9a3d2661e1a1062ac5afc7e3622668a917bfe4
unknown <szaicadmin@kemh.szaic.gov.cn>
2021-01-25 21:26:45 +0800

    完成了登录和注册页面的开发

--- assets/js/login.js
+++ assets/js/login.js
@@ -11,6 +11,7 @@ $(function() {
 
     // 从layui中获取form对象
     var form = layui.form
+    var layer = layui.layer
         // 通过form.verify（）自定义校验规则
     form.verify({
         // 方式一：数组，只能有一条规则
@@ -19,7 +20,7 @@ $(function() {
         ],
         // 方式二：函数，可以有多条，规则和校验方式丰富
         repwd: function(value) {
-            var repwd = $('.layui-form-item[name=password]').val() //通过name属性取值
+            var repwd = $('.reg-box [name=password]').val() //通过name属性取值
             if (repwd != value) {
                 return '两次密码不一致'
             }
@@ -29,14 +30,37 @@ $(function() {
     //监听注册表单的提交事件
     $('#form_reg').on('submit', function(e) {
         e.preventDefault()
-        $.post('http://ajax.frontend.itheima.net/api/reguser', {
-            username: $('#form_reg[name=username]').val(),
-            password: $('#form_reg[name=password]').val()
-        }, function(res) {
+        var data = {
+            username: $('#form_reg [name=username]').val(),
+            password: $('#form_reg [name=password]').val()
+        }
+        $.post('/api/reguser', data, function(res) {
             if (res.status != 0) {
-                return console.log(res.message)
+                return layer.msg(res.message)
             }
-            console.log(res)
+            layer.msg(res.message + '请登录')
+            $('#link_login').click()
+        })
+    })
+
+    //监听登录表单的提交事件
+    $('#form_login').on('submit', function(e) {
+        e.preventDefault()
+
+        $.ajax({
+            type: "POST",
+            url: "/api/login",
+            data: $(this).serialize(),
+
+            success: function(res) {
+                if (res.status != 0) {
+                    return layer.msg(res.message)
+                }
+                layer.msg(res.message)
+                console.log(res.token)
+                localStorage.setItem('token', res.token)
+                location.href = "/index.html"
+            }
         })
     })
 })
