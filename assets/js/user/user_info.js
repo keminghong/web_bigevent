2145426e123a5fbe4390c8de77ef6feaea0313d2
keminghong <459054467@qq.com>
2021-02-02 00:13:36 +0800

    进度保存3.5重置密码

---
+++ assets/js/user/user_info.js
@@ -0,0 +1,50 @@
+$(function() {
+    var form = layui.form
+    var layer = layui.layer
+    form.verify({
+        nickname: function(value) {
+            if (value.length > 6) {
+                return '昵称长度必须要在1-6个字符之间！'
+            }
+        }
+    })
+
+    initUserInfo()
+
+    //初始化用户信息
+    function initUserInfo() {
+        $.ajax({
+            type: "GET",
+            url: "/my/userinfo",
+            success: function(res) {
+                if (res.status != 0) {
+                    return layui.msg('获取用户信息失败！')
+                }
+
+                //调用form.val()快速为表单赋值
+                form.val('formUserInfo', res.data)
+            }
+        })
+    }
+    $('#btnReset').on('click', function(e) {
+        e.preventDefault()
+        initUserInfo()
+    })
+
+    $('.layui-form').on('submit', function(e) {
+        e.preventDefault()
+        $.ajax({
+            type: "POST",
+            url: "/my/userinfo",
+            data: $(this).serialize(),
+            success: function(res) {
+                if (res.status != 0) {
+                    return layer.msg('更新用户信息失败！')
+                }
+                layer.msg('更新用户信息成功')
+                window.parent.getUserInfo()
+
+            }
+        })
+    })
+})
