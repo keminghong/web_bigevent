75698dfb44c46fc989b7ec7d006a2564574ac4ee
keminghong <459054467@qq.com>
2021-02-03 17:55:05 +0800

    3-8文章列功能开发完成

---
+++ assets/js/article/art_list.js
@@ -0,0 +1,70 @@
+$(function() {
+    var layer = layui.layer
+    var form = layui.form
+        // 定义美化时间的过滤器
+    template.defaults.imports.dataFormat = function(date) {
+        const dt = new Date(date)
+
+        var y = dt.getFullYear()
+        var m = padZero(dt.getMonth() + 1)
+        var d = padZero(dt.getDate())
+
+        var hh = padZero(dt.getHours())
+        var mm = padZero(dt.getMinutes())
+        var ss = padZero(dt.getSeconds())
+
+        return y + '-' + m + '-' + d + ' ' + hh + ':' + mm + ':' + ss
+    }
+
+    // 定义补零的函数
+    function padZero(n) {
+        return n > 9 ? n : '0' + n
+    }
+    var q = {
+        pagenum: 1, //	int	页码值
+        pagesize: 2, //每页显示多少条数据
+        cate_id: '', //文章分类的 Id
+        state: '' //文章的状态，可选值有：已发布、草稿
+    }
+    initTable()
+    initCate()
+
+    function initTable() {
+        $.ajax({
+            type: "GET",
+            url: "/my/article/list",
+            data: q,
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg('获取文章列表失败！')
+                }
+                var htmlStr = template('tpl_table', res)
+                $('tbody').html(htmlStr)
+            }
+        })
+    }
+
+    function initCate() {
+        $.ajax({
+            type: "GET",
+            url: "/my/article/cates",
+
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg('获取分类数据失败！')
+                }
+                var htmlStr = template('tpl_cate', res)
+                $('[name=cate_id]').html(htmlStr)
+                form.render()
+            }
+        })
+    }
+    $('#form-search').on('submit', function(e) {
+        e.preventDefault()
+        var cate_id = $('[name=cate_id]').val()
+        var state = $('[name=state]').val()
+        q.cate_id = cate_id
+        q.state = state
+        initTable()
+    })
+})
